0b67200b2f78198736104a4aa008c0bfdfd6f2de
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-28 17:08:17 +0300

    Add project files.

---
+++ BigData/Controllers/DefaultController.cs
@@ -0,0 +1,31 @@
+﻿
+using BigData.DAL.Entities;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Numerics;
+
+namespace BigData.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly string _connectionString;
+
+        public DefaultController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IActionResult GetAllCars()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                string sqlQuery = "SELECT TOP 10 * FROM Plates"; 
+                IEnumerable<Plates> cars = connection.Query<Plates>(sqlQuery);
+                return View(cars);
+            }
+        }
+
+    }
+}
