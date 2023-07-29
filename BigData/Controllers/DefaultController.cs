147f70bf3d5f5b1c37dbefc6935fcee75f3b9328
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-29 20:42:04 +0300

    sayaflama

--- BigData/Controllers/DefaultController.cs
+++ BigData/Controllers/DefaultController.cs
@@ -1,29 +1,63 @@
-﻿
-using BigData.DAL.Entities;
+﻿using BigData.DAL.Entities;
 using Dapper;
 using Microsoft.AspNetCore.Mvc;
-using System.Data;
 using System.Data.SqlClient;
-using System.Numerics;
+using Microsoft.Extensions.Configuration;
 
 namespace BigData.Controllers
 {
     public class DefaultController : Controller
     {
         private readonly string _connectionString;
+        private readonly IConfiguration _configuration;
 
         public DefaultController(IConfiguration configuration)
         {
-            _connectionString = configuration.GetConnectionString("DefaultConnection");
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
         }
 
-        public IActionResult GetAllCars(int pageNumber = 1, int pageSize = 50)
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private int GetTotalCarCount(SqlConnection connection)
+        {
+            string countQuery = "SELECT COUNT(*) FROM Plates";
+            int totalCount = connection.ExecuteScalar<int?>(countQuery) ?? 0; // Default değeri 0 olarak ayarladık.
+            return totalCount;
+        }
+
+        public IActionResult GetAllCars(int pageNumber = 1, int pageSize = 25, string searchText = "")
         {
             using (var connection = new SqlConnection(_connectionString))
             {
                 int offset = (pageNumber - 1) * pageSize;
-                string sqlQuery = $"SELECT * FROM Plates ORDER BY ID OFFSET {offset} ROWS FETCH NEXT {pageSize} ROWS ONLY";
-                IEnumerable<Plates> cars = connection.Query<Plates>(sqlQuery);
+
+                // Arama metnini kullanarak parametreli bir sorgu oluşturun
+                string whereClause = string.IsNullOrEmpty(searchText)
+                    ? "" // Eğer arama metni boş ise tüm verileri getir
+                    : @"WHERE PLATE LIKE @SearchText
+                    OR LICENCEDATE LIKE @SearchText
+                    OR TITLE LIKE @SearchText
+                    OR BRAND LIKE @SearchText
+                    OR MODEL LIKE @SearchText
+                    OR YEAR_ LIKE @SearchText
+                    OR FUEL LIKE @SearchText
+                    OR SHIFTTYPE LIKE @SearchText
+                    OR MOTORVOLUME LIKE @SearchText
+                    OR MOTORPOWER LIKE @SearchText
+                    OR COLOR LIKE @SearchText
+                    OR CASETYPE LIKE @SearchText"; // Diğer alanlar burada eklenmeli
+
+                string sqlQuery = $@"
+                    SELECT * FROM Plates
+                    {whereClause}
+                    ORDER BY ID OFFSET {offset} ROWS FETCH NEXT {pageSize} ROWS ONLY";
+
+                // Parametreli sorguyu kullanmak için parametreleri ekleyin
+                IEnumerable<Plates> cars = connection.Query<Plates>(sqlQuery, new { SearchText = $"%{searchText}%" });
 
                 ViewBag.PageNumber = pageNumber;
                 ViewBag.PageSize = pageSize;
@@ -31,12 +65,6 @@ namespace BigData.Controllers
 
                 return View(cars);
             }
-        }
-
-        private int GetTotalCarCount(SqlConnection connection)
-        {
-            string countQuery = "SELECT COUNT(*) FROM Plates";
-            return connection.ExecuteScalar<int>(countQuery);
         }
     }
 }
