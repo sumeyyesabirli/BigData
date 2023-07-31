303fea89aeaa7a50c6126f1920abe6b49da22d39
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-31 21:50:18 +0300

    güncellemeler

--- BigData/Controllers/DefaultController.cs
+++ BigData/Controllers/DefaultController.cs
@@ -2,7 +2,6 @@
 using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
 
 namespace BigData.Controllers
 {
@@ -25,7 +24,7 @@ namespace BigData.Controllers
         private int GetTotalCarCount(SqlConnection connection)
         {
             string countQuery = "SELECT COUNT(*) FROM Plates";
-            int totalCount = connection.ExecuteScalar<int?>(countQuery) ?? 0; 
+            int totalCount = connection.ExecuteScalar<int?>(countQuery) ?? 0;
             return totalCount;
         }
 
@@ -35,36 +34,61 @@ namespace BigData.Controllers
             {
                 int offset = (pageNumber - 1) * pageSize;
 
-                
                 string whereClause = string.IsNullOrEmpty(searchText)
-                    ? "" 
+                    ? ""
                     : @"WHERE PLATE LIKE @SearchText
-                    OR LICENCEDATE LIKE @SearchText
-                    OR TITLE LIKE @SearchText
-                    OR BRAND LIKE @SearchText
-                    OR MODEL LIKE @SearchText
-                    OR YEAR_ LIKE @SearchText
-                    OR FUEL LIKE @SearchText
-                    OR SHIFTTYPE LIKE @SearchText
-                    OR MOTORVOLUME LIKE @SearchText
-                    OR MOTORPOWER LIKE @SearchText
-                    OR COLOR LIKE @SearchText
-                    OR CASETYPE LIKE @SearchText";
+            OR LICENCEDATE LIKE @SearchText
+            OR TITLE LIKE @SearchText
+            OR BRAND LIKE @SearchText
+            OR MODEL LIKE @SearchText
+            OR YEAR_ LIKE @SearchText
+            OR FUEL LIKE @SearchText
+            OR SHIFTTYPE LIKE @SearchText
+            OR MOTORVOLUME LIKE @SearchText
+            OR MOTORPOWER LIKE @SearchText
+            OR COLOR LIKE @SearchText
+            OR CASETYPE LIKE @SearchText";
 
                 string sqlQuery = $@"
-                    SELECT * FROM Plates
-                    {whereClause}
-                    ORDER BY ID OFFSET {offset} ROWS FETCH NEXT {pageSize} ROWS ONLY";
+            SELECT * FROM Plates
+            {whereClause}
+            ORDER BY ID OFFSET {offset} ROWS FETCH NEXT {pageSize} ROWS ONLY";
 
-                
                 IEnumerable<Plates> cars = connection.Query<Plates>(sqlQuery, new { SearchText = $"%{searchText}%" });
 
                 ViewBag.PageNumber = pageNumber;
                 ViewBag.PageSize = pageSize;
-                ViewBag.TotalCount = GetTotalCarCount(connection);
+
+                // Get the total count of all cars
+                string totalCountQuery = "SELECT COUNT(*) FROM PLATES " + whereClause;
+                int totalCount = connection.ExecuteScalar<int>(totalCountQuery, new { SearchText = $"%{searchText}%" });
+                ViewBag.TotalCount = totalCount;
+
+                // Get brand with the maximum count and its count
+                string brandMaxQuery = "SELECT TOP 1 BRAND, COUNT(*) AS Count FROM Plates GROUP BY BRAND ORDER BY Count DESC";
+                var brandMax = connection.QueryFirstOrDefault<BrandCountModel>(brandMaxQuery);
+                ViewBag.BrandMax = brandMax?.BRAND;
+                ViewBag.CountMax = brandMax?.COUNT;
+
+                string colorMaxQuery = "SELECT TOP 1 COLOR, COUNT(*) AS Count FROM Plates GROUP BY COLOR ORDER BY Count DESC";
+                var colorMax = connection.QueryFirstOrDefault<BrandCountModel>(colorMaxQuery);
+                ViewBag.ColorMax = colorMax?.COLOR;
+                ViewBag.CountColorMax = colorMax?.COUNT;
+
+                string shiftTypeMaxQuery = "SELECT TOP 1 SHIFTTYPE, COUNT(*) AS Count FROM Plates GROUP BY SHIFTTYPE ORDER BY Count DESC";
+                var shiftTypeMax = connection.QueryFirstOrDefault<BrandCountModel>(shiftTypeMaxQuery);
+                ViewBag.ShiftTypeMax = shiftTypeMax?.SHIFTTYPE;
+                ViewBag.CountShiftTypeMax = shiftTypeMax?.COUNT;
+
+       
+
 
                 return View(cars);
+
+            
+
             }
         }
+
     }
 }
---
+++ BigData/DAL/Entities/BrandCountModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BigData.DAL.Entities
+{
+    public class BrandCountModel
+    {
+        public string BRAND { get; set; }
+        public int COUNT { get; set; }
+        public string COLOR { get; set; }
+        public string SHIFTTYPE { get; set; }
+    }
+}
